184f169ed6dd69fc7befca31b3ca525768a773f3
ashiq <ashiq@dotserp.com>
2026-01-31 00:41:47 +0530

    implemented seen and delivered feature

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -18,6 +18,9 @@ interface chatMessage {
 
   username: string;
   text: string;
+  seenAt: Date;
+  createdAt: Date;
+  status: string;
 }
 
 interface Sender {
@@ -30,18 +33,21 @@ interface ServerChatMessage {
   _id: string;
   text: string;
   senderId: Sender;
+  seenAt: Date;
+  createdAt: Date;
+  status: string;
 }
 dayjs.extend(relativeTime);
 const Chat = () => {
   const { targetUserId } = useParams();
   const [messages, setMessages] = useState<chatMessage[]>([]);
   const [newmessages, setNewMessages] = useState("");
+  const [lastSeen, setLastSeen] = useState<string>("");
   const [isOnline, setIsOnline] = useState<boolean>(false);
   const [currentChatingUser, setCurrentChatingUser] = useState<
     Partial<UserState>
   >({});
   const user = useSelector((state: RootState) => state.user);
-  const lastSeeen: string = "2026-01-28T00:00:00.000Z";
 
   const userId = user?._id;
 
@@ -55,6 +61,8 @@ const Chat = () => {
     }
   }
   const sendChat = () => {
+    console.log("hi  ");
+
     if (!newmessages.trim()) return;
     socketRef.current?.emit("sendMessage", {
       userId,
@@ -82,11 +90,29 @@ const Chat = () => {
             userId: _id,
             username: `${firstName} ${lastName}`,
             text: msg?.text,
+            seenAt: msg?.seenAt,
+            createdAt: msg?.createdAt,
+            status: msg?.status,
           };
         });
 
+        console.log(chatMassages);
+
         setMessages(chatMassages);
       }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchLastSeen = async () => {
+    try {
+      const res = await axios.get(BASE_URL + `/user/${targetUserId}/status`, {
+        withCredentials: true,
+      });
+
+      console.log(res?.data?.lastSeen);
+      setLastSeen(res?.data?.lastSeen);
     } catch (err) {
       console.log(err);
     }
@@ -107,13 +133,15 @@ const Chat = () => {
     if (!targetUserId) return;
     fetchTargetUserDetails();
     fetchAllChat();
+    fetchLastSeen();
   }, [targetUserId]);
 
   useEffect(() => {
     if (!userId || !targetUserId) return;
+    const socket = createSocketConnection();
+    socketRef.current = socket;
 
-    socketRef.current = createSocketConnection();
-    socketRef.current?.on("online-users", (users) => {
+    socket.on("online-users", (users) => {
       console.log(users);
 
       if (users.includes(targetUserId)) {
@@ -122,24 +150,42 @@ const Chat = () => {
         setIsOnline(false);
       }
     });
+    socket.on("connect", () => {
+      console.log("FRONTEND CONNECTED:", socket.id);
+
+      console.log("EMITTING joinChat");
+      socket.emit("joinChat", {
+        userId,
+        targetUserId,
+        username: `${user?.firstName} ${user?.lastName}`,
+      });
+    });
+
     // socketRef.current?.on("user-online", (id) => {
     //   console.log("logined user id", id);
     //   if (id === targetUserId) {
     //     setIsOnline(true);
     //   }
     // });
-    socketRef.current?.emit("joinChat", {
-      userId,
-      targetUserId,
-      username: `${user?.firstName} ${user?.lastName}`,
-    });
+    console.log("h");
 
-    socketRef.current?.on("chatMessage", (msg) => {
-      setMessages((prev) => [...prev, msg]);
+    console.log("hbbbb");
+
+    socket.on("chatMessage", (msg) => {
+      console.log("dddd");
+
+      console.log(msg);
+
+      setMessages((prev) => {
+        const updated = [...prev, msg];
+        console.log(updated);
+        return updated;
+      });
     });
 
     return () => {
-      socketRef.current?.off("chatMessage");
+      socket.off("chatMessage");
+      socket.off("online-users");
 
       socketRef.current?.disconnect();
     };
@@ -151,45 +197,39 @@ const Chat = () => {
 
   return (
     <div className="max-w-4xl m-auto border border-gray-50  flex flex-col min-h-[70vh]">
-     <div className="border-b border-gray-500/50 border-opacity-10 p-5">
-  <div className="flex items-center gap-3">
+      <div className="border-b border-gray-500/50 border-opacity-10 p-5">
+        <div className="flex items-center gap-3">
+          {/* Avatar */}
+          <div className="w-10 h-10 rounded-full overflow-hidden">
+            <img
+              src={currentChatingUser?.photoUrl || "/placeholder.jpg"}
+              alt="user avatar"
+              className="w-full h-full object-cover"
+            />
+          </div>
 
-    {/* Avatar */}
-    <div className="w-10 h-10 rounded-full overflow-hidden">
-      <img
-        src={currentChatingUser?.photoUrl || "/placeholder.jpg"}
-        alt="user avatar"
-        className="w-full h-full object-cover"
-      />
-    </div>
+          {/* Text Section */}
+          <div className="flex flex-col">
+            {/* Name + Status Dot */}
+            <div className="flex items-center gap-2">
+              <h1 className="font-extrabold text-lg leading-none">
+                {currentChatingUser?.firstName} {currentChatingUser?.lastName}
+              </h1>
 
-    {/* Text Section */}
-    <div className="flex flex-col">
+              <span
+                className={`w-2 h-2 rounded-full ${
+                  isOnline ? "bg-green-500" : "bg-red-500"
+                }`}
+              />
+            </div>
 
-      {/* Name + Status Dot */}
-      <div className="flex items-center gap-2">
-        <h1 className="font-extrabold text-lg leading-none">
-          {currentChatingUser?.firstName} {currentChatingUser?.lastName}
-        </h1>
-
-        <span
-          className={`w-2 h-2 rounded-full ${
-            isOnline ? "bg-green-500" : "bg-red-500"
-          }`}
-        />
+            {/* Online / Last seen */}
+            <span className="text-sm text-gray-400">
+              {isOnline ? "Online" : `Last seen: ${dayjs(lastSeen).fromNow()}`}
+            </span>
+          </div>
+        </div>
       </div>
-
-      {/* Online / Last seen */}
-      <span className="text-sm text-gray-400">
-        {isOnline
-          ? "Online"
-          : `Last seen: ${dayjs(lastSeeen).fromNow()}`}
-      </span>
-
-    </div>
-
-  </div>
-</div>
 
       <div
         className="p-2 flex-1 overflow-y-auto max-h-[60vh] "
@@ -201,12 +241,12 @@ const Chat = () => {
             <div className="chat chat-end" key={index}>
               <div className="flex items-center gap-2 group">
                 <time className="text-xs opacity-0  group-hover:opacity-50">
-                  12:46
+                  {dayjs(item?.createdAt).format("h:mm A")}
                 </time>
                 <div className="chat-bubble ">{item?.text}</div>
               </div>
 
-              <div className="chat-footer opacity-50">Seen at 12:46</div>
+              <div className="chat-footer opacity-50">{item?.status}</div>
             </div>
           ) : (
             <div className="chat chat-start" key={index}>
